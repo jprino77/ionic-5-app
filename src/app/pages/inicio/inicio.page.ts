0f1c2f47ff49803002a0e06abd82d7e37331c80d
Julian Prino <julianprino@gmail.com>
2019-09-19 04:34:25 -0300

    pruebas de header y list

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss']
+})
+export class InicioPage implements OnInit {
+  componentes: Componente[] = [{
+    icon: 'american-football',
+    name: 'Action Sheet',
+    redirectTo: '/action-sheet'
+  },{
+    icon: 'appstore',
+    name: 'Alert',
+    redirectTo: '/alert'}
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
